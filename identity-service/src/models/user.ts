bf7ea3f5134227d62e823ba0f51a3946a2936dab
Nadya <nb1985@ukr.net>
2025-05-05 10:36:04 +0300

    feat(identity-service): create MongoDB schema for Users

---
+++ identity-service/src/models/user.ts
@@ -0,0 +1,56 @@
+import { model } from "mongoose";
+
+
+const mongoose = require("mongoose");
+const argon2 = require("argon2");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (this: IUser, next: Function) {
+  if (this.isModified("password")) {
+    try {
+      this.password = await argon2.hash(this.password);
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  try {
+    return await argon2.verify(this.password, candidatePassword);
+  } catch (error) {
+    throw new Error("Password doesn't match, please try again");
+  }
+};
+
+userSchema.index({username: "text"});
+
+const User = model("User", userSchema);
+
+module.exports = User;

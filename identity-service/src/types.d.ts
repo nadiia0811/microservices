d179f3f5a69bc7d2bb178839a5bb8bcfca790476
Nadya <nb1985@ukr.net>
2025-05-11 14:04:36 +0300

    feat(identity-service): add comparePassword property to IUser type for safety

--- identity-service/src/types.d.ts
+++ identity-service/src/types.d.ts
@@ -2,14 +2,17 @@ import { Document } from "mongoose";
 
 declare global {
     interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date; 
-    comparePassword(candidatePassword: string):  Promise<boolean>
+      _id: mongoose.Schema.Types.ObjectId | string;    
+      username: string;
+      email: string;
+      password: string;
+      createdAt: Date;
+      updatedAt: Date; 
+      comparePassword(candidatePassword: string):  Promise<boolean>;
    }
 
    type RegisterUser = Pick<IUser, "username" | "email" | "password">;
+
+   type LoginUser = Pick<IUser, "email" | "password">;
 }
 

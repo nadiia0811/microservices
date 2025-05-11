510a9110d873661236f50c5ac103fb4670e1ab9e
Nadya <nb1985@ukr.net>
2025-05-11 14:06:24 +0300

    feat(identity-service): add loginUser validation

--- identity-service/src/utils/validation.ts
+++ identity-service/src/utils/validation.ts
@@ -1,7 +1,6 @@
-//const Joi = require("joi");
 import Joi from "joi";
 
- export const validateRegistration = (data: RegisterUser) => {
+export const validateRegistration = (data: RegisterUser) => {
   const schema = Joi.object({
     username: Joi.string().min(3).max(50).required(),
     email: Joi.string().email().required(),
@@ -11,3 +10,11 @@ import Joi from "joi";
   return schema.validate(data);
 };
 
+export const validateLogin = (data: LoginUser) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+  });
+
+  return schema.validate(data);
+};

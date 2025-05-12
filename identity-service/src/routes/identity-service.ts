64908d4b975d073912f0ccb718c2abb6583718b7
Nadya <nb1985@ukr.net>
2025-05-12 10:19:48 +0300

    feat(identity-service): implement logout and refresh token routes

--- identity-service/src/routes/identity-service.ts
+++ identity-service/src/routes/identity-service.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/identity-controller";
+ import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshTokenUser
+} from "../controllers/identity-controller"; 
+ 
 
 const router = express.Router();
 //@ts-ignore
 router.post("/register", registerUser);
 //@ts-ignore
 router.post("/login", loginUser);
+//@ts-ignore
+router.post("/logout", logoutUser);
+//@ts-ignore
+router.post("/refresh-token", refreshTokenUser);
 
 export default router;
